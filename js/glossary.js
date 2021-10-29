c231cf8b9cbfd59cc059cd0db77eb394b82ba34d
chiahao chou <chiahao.codes@gmail.com>
2021-10-28 20:59:40 -0700

    added functions inside main.js

--- js/glossary.js
+++ js/glossary.js
@@ -1,76 +1,75 @@
-document.addEventListener("readystatechange", () => {
-    if (document.readyState == "loading") {
-    return console.log("DOM loading not complete.");
-    } else {
-      console.log("Glossary.js readyState:", document.readyState);
-     const piece = {
-       empty: 0,
-       wPwn: 1,
-       wNight: 2,
-       wBshp: 3,
-       wRook: 4,
-       wQueen: 5,
-       wKing: 6,
-       bPwn: 7,
-       bNight: 8,
-       bBshp: 9,
-       bRook: 10,
-       bQueen: 11,
-       bKing: 12,
-     };
-     const boardSq = 120;
-     const file = {
-       fileA: 0,
-       fileB: 1,
-       fileC: 2,
-       fileD: 3,
-       fileE: 4,
-       fileF: 5,
-       fileG: 6,
-       fileH: 7,
-       fileNone: 8,
-     };
-     const rank = {
-       rank1: 0,
-       rank2: 1,
-       rank3: 2,
-       rank4: 3,
-       rank5: 4,
-       rank6: 5,
-       rank7: 6,
-       rank8: 7,
-       rankNone: 8,
-     };
-     const colours = { white: 0, black: 1, both: 2 };
-     const squares = {
-       a1: 21,
-       b1: 22,
-       c1: 23,
-       d1: 24,
-       e1: 25,
-       f1: 26,
-       g1: 27,
-       h1: 28,
-       a8: 91,
-       b8: 92,
-       c8: 93,
-       d8: 94,
-       e8: 95,
-       f8: 96,
-       g8: 97,
-       h8: 98,
-       no_squ: 99,
-       offboard: 100,
-     }; //squares represent key positions on the chess board (i.e. white/black home file and rank);
+  var pieces = {
+    empty: 0,
+    wPwn: 1,
+    wNight: 2,
+    wBshp: 3,
+    wRook: 4,
+    wQueen: 5,
+    wKing: 6,
+    bPwn: 7,
+    bNight: 8,
+    bBshp: 9,
+    bRook: 10,
+    bQueen: 11,
+    bKing: 12,
+  };
+  var boardSq = 120;
+  var files = {
+    fileA: 0,
+    fileB: 1,
+    fileC: 2,
+    fileD: 3,
+    fileE: 4,
+    fileF: 5,
+    fileG: 6,
+    fileH: 7,
+    fileNone: 8,
+  };
+  var ranks = {
+    rank1: 0,
+    rank2: 1,
+    rank3: 2,
+    rank4: 3,
+    rank5: 4,
+    rank6: 5,
+    rank7: 6,
+    rank8: 7,
+    rankNone: 8,
+  };
+  var colours = { white: 0, black: 1, both: 2 };
 
-      const bool = { false: 0, true: 1 };
-      const fileBoard = new Array(boardSq); //file array;
-      const rankBoard = new Array(boardSq); //rank array;
+  //squares numbered as per Number Board in google sheets;
+  //these values represent the rank/file number at each square on the chess board;
+  //extra rank/file rows factored in;
+  var squares = {
+    a1: 21,
+    b1: 22,
+    c1: 23,
+    d1: 24,
+    e1: 25,
+    f1: 26,
+    g1: 27,
+    h1: 28,
+    a8: 91,
+    b8: 92,
+    c8: 93,
+    d8: 94,
+    e8: 95,
+    f8: 96,
+    g8: 97,
+    h8: 98,
+    no_squ: 99,
+    offboard: 100,
+  }; //squares represent key positions on the chess board (i.e. white/black home file and rank);
 
-      
-      
-      
-    return;
+  var bool = { false: 0, true: 1 };
+  var fileBoard = new Array(boardSq); //file array;
+  var rankBoard = new Array(boardSq); //rank array;
+
+  //converts the file/rank position into the Numbered position on the chess board;
+  function fileRankSqu(f, r) {
+    return 21 + f + r * 10;
   }
-  
-});
+
+
+
--- js/main.js
+++ js/main.js
@@ -1,7 +1,35 @@
-document.addEventListener("readystatechange", () => {
-    if (document.readyState == "loading") {
-        console.log("DOM is still loading.");
-    } else {
-        console.log("main.js readyState:",document.readyState);
+initFileRankBrd();
+
+function initFileRankBrd() {
+  var index;
+  var file = files.fileA;
+  var rank = ranks.rank1;
+  var squ = squares.a1;
+
+  //set each square in the File Board and Rank Board to 100 aka..offboard;
+  //google sheets contains illustrations for File/Rank Board;
+  for (index = 0; index < boardSq; ++index) {
+    fileBoard[index] = squares.offboard;
+    rankBoard[index] = squares.offboard;
+  }
+
+  for (rank; rank <= ranks.rank8; ++rank) {
+    for (file; file <= files.fileH; ++file) {
+      squ = fileRankSqu(file, rank);
+      fileBoard[squ] = file;
+      rankBoard[squ] = rank;
     }
-});
+  }
+
+  console.log(`fileBoard[0]: ${fileBoard[0]}, rankBoard[0]:${rankBoard[0]}`);
+  console.log(
+    `fileBoard[squ]: ${fileBoard[squ]}, rankBoard[square.a1]: ${rankBoard[squ]}`
+  );
+  console.log(
+    `fileBoard[square.e8]: ${fileBoard[squares.e8]}, rankBoard[square.e8]: ${
+      rankBoard[squares.e8]
+    }`
+    );
+}
+
+   
