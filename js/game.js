81664d68745da4e0b601757a56680f078971635b
chiahao chou <chiahao.codes@gmail.com>
2021-12-08 13:18:07 -0800

    modified the for loop

--- js/game.js
+++ js/game.js
@@ -1,18 +1,24 @@
 function myChessFile() {
   const { Chess } = require("chess.js");
   const chessGame = new Chess();
-  chessGame.clear();
   console.log(chessGame.ascii());
 
   const myDomBoard = document.getElementsByClassName("chessboard");
   const rankFile = myDomBoard[0].children;
   console.log(rankFile); //HtmlCollection of rankFile arrays, live;
 
-  for (i = 0; i <= 7; i++){
-    for (j = 0; j <= 7; j++){
-      rankFile[i].children[j].addEventListener("click", () => {
-        console.log("Hey...");
-      })
+  for (i = 0; i <= 7; i++) {
+    for (j = 0; j <= 7; j++) {
+      const squares = rankFile[i].children[j];
+      squares.addEventListener("click", () => {
+        squares.classList.add("currentMove");
+        if (squares.classList.contains("currentMove")) {
+          const squareInnerHtml = squares.innerHTML;
+          console.log(squareInnerHtml);
+          const moves = chessGame.moves({square:squareInnerHtml});
+          console.log(moves);
+        }
+      });
     }
   }
 }
