5c914622b062fd02ae332371ba83d927ce7bcba8
chiahao chou <chiahao.codes@gmail.com>
2021-12-09 00:13:57 -0800

    added to js functionality, added class name and images in css

--- bundle.js
+++ bundle.js
@@ -10,14 +10,43 @@ function myChessFile() {
 
   for (i = 0; i <= 7; i++) {
     for (j = 0; j <= 7; j++) {
-      const squares = rankFile[i].children[j];
-      squares.addEventListener("click", () => {
-        squares.classList.add("currentMove");
-        if (squares.classList.contains("currentMove")) {
-          const squareInnerHtml = squares.innerHTML;
-          console.log(squareInnerHtml);
-          const moves = chessGame.moves({square:squareInnerHtml});
-          console.log(moves);
+      const mySquares = rankFile[i].children[j];
+      mySquares.addEventListener("click", () => {
+
+        //clear out all other class names first;
+        const otherSquares = document.querySelectorAll(
+          ".rankFile > .currentMove"
+        );
+        otherSquares.forEach((ele) => {
+          ele.classList.remove("currentMove");
+        });
+        const existingValidMoves = document.querySelectorAll(".rankFile > .validMove");
+        existingValidMoves.forEach((ele) => ele.classList.remove("validMove"));
+
+        //add class name to square being clicked;
+        mySquares.classList.add("currentMove");
+
+        //calculate eligible moves;
+        let moves;
+        if (mySquares.classList.contains("currentMove")) {
+          const squareInnerHtml = mySquares.innerHTML;
+          moves = chessGame.moves({ square: squareInnerHtml }); //array;
+          if (moves.length > 0) {
+            console.log(moves);
+
+            // highlight the squares of the available moves;
+            const allSquares = document.querySelectorAll(".rankFile > div"); //static nodeList array-like;
+            for (let h of allSquares) {
+              for (let k of moves) {
+                if (h.innerHTML === k) {
+                  console.log("Good Luck!");
+                  h.classList.add("validMove");
+                }
+              }
+            }
+          } else {
+            console.log("Invalid move.");
+          }
         }
       });
     }
