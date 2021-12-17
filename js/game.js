4b47cc1747ada817a38c438b298a09a16637d0f6
chiahao chou <chiahao.codes@gmail.com>
2021-12-16 21:44:42 -0800

    modified js files

--- js/game.js
+++ js/game.js
@@ -3,6 +3,10 @@ function myChessFile() {
   const chessGame = new Chess();
   console.log(chessGame.ascii());
 
+  window.addEventListener("load", () => {
+    console.log("Game Board loaded. White's turn.");
+  });
+
   const myDomBoard = document.getElementsByClassName("chessboard");
   const rankFile = myDomBoard[0].children;
   console.log(rankFile); //HtmlCollection of rankFile arrays, live;
@@ -27,75 +31,132 @@ function myChessFile() {
         //add currentMove class to square being clicked;
         mySquares.classList.add("currentMove");
 
-        //calculate eligible moves;
-        let moves;
-        const allSquares = document.querySelectorAll(".rankFile > div");
-        const squareInnerHtml = mySquares.innerHTML;
-
         if (mySquares.classList.contains("currentMove")) {
-          moves = chessGame.moves({ square: squareInnerHtml }); //array;
+          let moves;
+          const allSquares = document.querySelectorAll(".rankFile > div");
+          const squareInnerHtml = mySquares.innerHTML;
+          moves = chessGame.moves({ square: squareInnerHtml, verbose: true }); //array;
+
           if (moves.length > 0) {
-            console.log(moves);
-            // highlight the squares of the available moves;
             for (let h of allSquares) {
               for (let k of moves) {
-                if (
-                  h.innerHTML === k ||
-                  h.innerHTML === k.substring(1) ||
-                  h.innerHTML === k.substring(2) ||
-                  h.innerHTML === k.substring(1, 3) ||
-                  h.innerHTML === k.substring(1, 4) ||
-                  h.innerHTML === k.substring(2, 4) ||
-                  h.innerHTML === k.substring(2, 3)
-                ) {
-                  console.log("Good Luck!");
+                if (h.innerHTML === k.to) {
                   h.classList.add("validMove");
                 }
               }
             }
-            const validMoves = document.querySelectorAll(".validMove");
-            const currentMove2 = document.querySelector(".currentMove");
-            const arrPieces = [
-              "p",
-              "b",
-              "q",
-              "n",
-              "k",
-              "r",
-              "P",
-              "R",
-              "N",
-              "B",
-              "Q",
-              "K",
-            ];
-            if (validMoves.length > 0) {
-              validMoves.forEach((ele) => {
-                ele.addEventListener("click", () => {
-                  //remove chess piece class name and add it to ele.classList;
-                  currentMove2.classList.forEach((elem) => {
-                    if (arrPieces.includes(elem)) {
-                      currentMove2.classList.remove(elem);
-                      ele.classList.replace("currentMove", elem);
-                      //update chess engine move();
-                      const chessMove = chessGame.move({
-                        from: currentMove2.innerHTML,
-                        to: ele.innerHTML,
-                      });
-                      if (chessMove.captured) {
-                        if (arrPieces.includes(ele.classList[1])) {
-                          ele.classList.remove(ele.classList[1]);
-                        }
+
+            const validMoves = document.querySelectorAll(".validMove"); //elements in a static node list;
+            console.log(validMoves);
+            let chessMove;
+
+            for (const validEle of validMoves) {
+              for (const movesObj of moves) {
+
+                function validClick() {
+                  if (movesObj.color === "w") {
+                    const whitePiece = movesObj.piece.toUpperCase();
+                    console.log(
+                      `Chess piece is ${whitePiece}, color is ${movesObj.color}`
+                    );
+
+                    validEle.classList.remove(
+                      "currentMove",
+                      "K",
+                      "Q",
+                      "N",
+                      "P",
+                      "B",
+                      "R",
+                      "q",
+                      "k",
+                      "b",
+                      "p",
+                      "n",
+                      "r"
+                    );
+                    validEle.classList.add(whitePiece);
+                    console.log(validEle);
+
+                    for (const sq of allSquares) {
+                      if (sq.innerHTML === movesObj.from) {
+                        sq.classList.remove(whitePiece);
+                        console.log(`${whitePiece} removed.`);
+                        break;
                       }
-                      console.log(chessMove);
                     }
-                  });
+
+                    //update chess.move();
+                    if (validEle.innerHTML === movesObj.to) {
+                      console.log(movesObj.from, movesObj.to);
+                      chessMove = chessGame.move(
+                        { from: movesObj.from, to: movesObj.to },
+                        {
+                          sloppy: true,
+                        }
+                      );
+
+                      if (
+                        Object.keys(chessMove).includes("captured") === true
+                      ) {
+                        validEle.classList.remove(chessMove.captured);
+                        console.log(`${chessMove.captured} has been captured!`);
+                      }
+                    }
+                  } else if (movesObj.color === "b") {
+                    console.log(
+                      `Chess piece is ${movesObj.piece}, color is ${movesObj.color}`
+                    );
+
+                    validEle.classList.remove(
+                      "currentMove",
+                      "q",
+                      "k",
+                      "b",
+                      "p",
+                      "n",
+                      "r",
+                      "K",
+                      "Q",
+                      "N",
+                      "P",
+                      "B",
+                      "R"
+                    );
+                    validEle.classList.add(movesObj.piece);
+                    console.log(validEle);
+
+                    for (const sq of allSquares) {
+                      if (sq.innerHTML === movesObj.from) {
+                        sq.classList.remove(movesObj.piece);
+                        break;
+                      }
+                    }
+
+                    if (validEle.innerHTML === movesObj.to) {
+                      console.log(movesObj.from, movesObj.to);
+                      chessMove = chessGame.move(
+                        { from: movesObj.from, to: movesObj.to },
+                        {
+                          sloppy: true,
+                        }
+                      );
+
+                      if (
+                        Object.keys(chessMove).includes("captured") === true
+                      ) {
+                        const capturedWhite = chessMove.captured.toUpperCase();
+                        validEle.classList.remove(capturedWhite);
+                        console.log(`${capturedWhite} has been captured!`);
+                      }
+                    }
+                  }
+                  console.log(chessGame.ascii());
                   const gameFen = chessGame.fen();
                   console.log(`Fen: ${gameFen}`);
                   const validFen = chessGame.validate_fen(gameFen);
                   if (!validFen) console.log("Invalid Fen", validFen);
-                  console.log(chessGame.ascii());
-                  let turn = chessGame.turn();
+
                   const inCheck = chessGame.in_check(gameFen);
                   const checkMate = chessGame.in_checkmate(gameFen);
                   const draw = chessGame.in_draw(gameFen);
@@ -105,7 +166,8 @@ function myChessFile() {
                     console.log("Game has ended.");
                   }
 
-                  if (turn == "b") {
+                  let turn = chessGame.turn();
+                  if (turn === "b") {
                     console.log("Black's turn.");
                     if (inCheck) {
                       console.log("Black in check.");
@@ -116,8 +178,7 @@ function myChessFile() {
                     } else if (staleMate) {
                       console.log("Black has been stalemated.");
                     }
-                  }
-                   else {
+                  } else {
                     console.log("White's turn.");
                     if (inCheck) {
                       console.log("White in check.");
@@ -129,8 +190,13 @@ function myChessFile() {
                       console.log("White has been stalemated.");
                     }
                   }
+                }
+
+                validEle.removeEventListener("click", validClick, {
+                  once: true,
                 });
-              });
+                validEle.addEventListener("click", validClick, { once: true });
+              }
             }
           }
         }
